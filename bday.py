848866320d0706ffbd37091b00a65dd9edab69d4
dhreetirayyy <dkumar.deo@gmail.com>
2025-02-21 20:40:23 -0800

    Writing 5 friends bday

---
+++ bday.py
@@ -0,0 +1,24 @@
+Rachel = "Feburary" 
+rachel = 28
+print("Rachel:")
+print(Rachel , rachel )
+print(" ")
+Maddi = "July" 
+maddi = 19
+print("Maddi:")
+print(Maddi , maddi)
+print(" ")
+Steven = "December" 
+steven = 5
+print("Steven:")
+print(Steven , steven)
+print(" ")
+Jacob = "May" 
+jacob = 30
+print("Jacob:")
+print(Jacob , jacob)
+print(" ")
+Eva = "August" 
+eva = 11
+print("Eva:")
+print(Eva , eva)
